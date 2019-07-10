bdd3a93b286da2ca016254f25b78d57a6de286ab
Christopher Bartling <chris.bartling@gmail.com>
2019-07-09 22:02:16 -0500

    Initial version of the Spring Boot sample app.

---
+++ opentracing-demo-springboot/src/main/java/com/pintailconsultingllc/opentracingdemospringboot/OpentracingDemoSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.opentracingdemospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentracingDemoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpentracingDemoSpringbootApplication.class, args);
+    }
+
+}
---
+++ opentracing-demo-springboot/src/test/java/com/pintailconsultingllc/opentracingdemospringboot/OpentracingDemoSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pintailconsultingllc.opentracingdemospringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpentracingDemoSpringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
