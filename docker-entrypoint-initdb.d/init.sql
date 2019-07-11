d886dfd6ca559135fcddb1e0e3d081de87d7be67
Christopher Bartling <chris.bartling@gmail.com>
2019-07-10 23:52:44 -0500

    Create new database user and grant privileges to both databases.

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE opentracing_demo_db2;
 
+CREATE USER 'opentracing_dev'@'%' IDENTIFIED BY 'opentracing';
+
+GRANT ALL ON opentracing_demo_db1.* TO 'opentracing_dev'@'%';
+
+GRANT ALL ON opentracing_demo_db2.* TO 'opentracing_dev'@'%';
+
+FLUSH PRIVILEGES;
+
