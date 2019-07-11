90b1402a5b89f15fdf21980f80f71e9e82755c03
Christopher Bartling <chris.bartling@gmail.com>
2019-07-11 01:01:03 -0500

    Enabling OpenTracing.

--- opentracing-demo-aspnetcore/Program.cs
+++ opentracing-demo-aspnetcore/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace opentracing_demo_aspnetcore
 {
@@ -18,7 +12,10 @@ namespace opentracing_demo_aspnetcore
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().ConfigureServices(services =>
+            {
+                // Enables and automatically starts the instrumentation!
+                services.AddOpenTracing();
+            });
     }
 }
