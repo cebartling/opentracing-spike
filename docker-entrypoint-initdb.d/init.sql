b12cc4fdf419d13fb028100a646b8436e5f113fc
Christopher Bartling <chris.bartling@gmail.com>
2019-07-11 21:47:26 -0500

    Adding tables.

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -8,3 +8,18 @@ GRANT ALL ON opentracing_demo_db2.* TO 'opentracing_dev'@'%';
 
 FLUSH PRIVILEGES;
 
+CREATE TABLE opentracing_demo_db1.participants (
+  id                INTEGER AUTO_INCREMENT      NOT NULL,
+  first_name        VARCHAR(40)                 NOT NULL,
+  last_name         VARCHAR(50)                 NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE opentracing_demo_db2.preferences (
+  id                INTEGER AUTO_INCREMENT      NOT NULL,
+  participant_id    INTEGER                     NOT NULL,
+  save_on_change    BOOLEAN DEFAULT 0           NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
